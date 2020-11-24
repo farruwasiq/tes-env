223962bd99dd7231414e7b472ac6c7d594820996
farruwasiq <farruwasiq@gmail.com>
2020-11-24 09:04:47 -0800

    adf'

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,9 @@
 provider "aws"{
-    region="us-east-1"
+    
 }
 resource "aws_instance" "test" {
     ami="ami-0885b1f6bd170450c"
+    availablility_zone="us-east-1a"
     instance_type="t2.nano"
     tags={
         name="farhan"
