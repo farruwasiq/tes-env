5fe10fe92814026c9e42c804a1f977fbbd0d83f8
farruwasiq <farruwasiq@gmail.com>
2020-11-28 10:09:49 -0800

    'removed

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ provider "aws"{
 }
 resource "aws_instance" "jenkins-test" {//creaing instance
     ami="ami-0885b1f6bd170450c"
-    availablility_zone="us-east-1a"
+    
     instance_type="t2.nano"
     tags={
         name="farhan"
