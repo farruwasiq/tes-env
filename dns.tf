1875d20347e0ddbed7a855fb7f421606aea624c4
farruwasiq <farruwasiq@gmail.com>
2020-11-28 16:49:46 -0800

    added records'

--- dns.tf
+++ dns.tf
@@ -1,17 +1,24 @@
 resource "aws_route53_zone" "my_zone"{
     name="farruwasiq.tk"
 }
-/*
-resource "aws_route53_record" "my-dns-record"{
+
+resource "aws_route53_record" "server-1-record"{
     zone_id=aws_route53_zone.my_zone.id
     name="blog.farruwasiq.tk"
     type="A"
     ttl="300"
-
-
+    records=["18.212.52.55"]
 
 }
-*/
+resource "aws_route53_record" "www-record"{
+    zone_id=aws_route53_zone.my_zone.id
+    name="www.farruwasiq.tk"
+    type="A"
+    ttl="300"
+    records = [ "54.147.39.231" ]
+}
+
+
 output "ns-servers" {
     value = aws_route53_zone.my_zone.name_servers
   
