ef3902876ec635307156364fce6d18e0cec5b6d0
farruwasiq <farruwasiq@gmail.com>
2020-11-28 16:41:29 -0800

    ns-output

--- dns.tf
+++ dns.tf
@@ -1,3 +1,18 @@
 resource "aws_route53_zone" "my_zone"{
     name="farruwasiq.tk"
 }
+/*
+resource "aws_route53_record" "my-dns-record"{
+    zone_id=aws_route53_zone.my_zone.id
+    name="blog.farruwasiq.tk"
+    type="A"
+    ttl="300"
+
+
+
+}
+*/
+output "ns-servers" {
+    value = aws_route53_zone.my_zone.name_servers
+  
+}
